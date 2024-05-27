0541761cdbdc6e9e420764285b8973d0b06df52d
SaranJoel <joel.david3721@gmail.com>
2024-05-27 14:41:46 -0400

    added tip cal

--- pythonProject/practice.py
+++ pythonProject/practice.py
@@ -18,6 +18,29 @@ False #they should start with Initial
 # print(f"your name has{num} numbers")
 #print("name has"+num+ "characters")  you cannot concatenate a string and integer
 
-two_digit_num= input()
-add= int(two_digit_num[0]) + int(two_digit_num[1])
-print(add)
+# two_digit_num= input()
+# add= int(two_digit_num[0]) + int(two_digit_num[1])
+# print(add)
+
+'''
+Welcome to tip calaculator
+what was the bill   ?
+How much tip would you like to give 10 12 or 15
+how many people to split the bill?
+each person should pay : this much
+'''
+
+#Variables
+# bill - float
+# tip - Int
+#billwith_tip
+# Split_bill - float
+#paybill = float
+
+bill = float(input("What was the bill?"))
+tip = int(input("How much tip would you like to give 10 12 or 15"))
+billwith_tip = (tip/100 *bill) + bill
+split_bill = int(input("Among how many people tp slit the bill?"))
+
+paybill = billwith_tip/split_bill
+print(f"each person should pay{round(paybill,2)}")
